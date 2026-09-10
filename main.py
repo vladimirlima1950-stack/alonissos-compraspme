b258d93587324010611b66009123c97f5ae62a58
Vladimir <vladimir.lima.1950@gmail.com>
2026-09-10 13:43:43 -0300

    alterado main.py

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ if __name__ == "__main__":
     import uvicorn
     # Inicia o servidor escutando a porta dinâmica atribuída pelo Railway ou 8000 localmente
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
